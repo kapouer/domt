48b6bd825bb804af4869653b7dd788ddcf6773ae
Jérémy Lal <kapouer@melix.org>
2014-05-19 10:25:14 +0200

    First tests

---
+++ test/run.js
@@ -0,0 +1,37 @@
+var jsdom = require("jsdom");
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+fs.readdirSync(__dirname).filter(function(file) {
+	if (path.extname(file) != ".html") return;
+	var str = fs.readFileSync(path.join(__dirname, file)).toString();
+	var doc = jsdom.jsdom(str, null, {features: {
+		FetchExternalResources   : false,
+		ProcessExternalResources : false,
+		MutationEvents           : false
+	}});
+	var win = doc.createWindow();
+	win.console = console;
+	win.describe = describe;
+	win.it = it;
+	win.assert = assert;
+	win.assert.html = function(id1, id2) {
+		assert.equal(
+			doc.querySelector('#'+id1).cloneNode().removeAttribute('id').outerHTML,
+			doc.querySelector('#'+id2).cloneNode().removeAttribute('id').outerHTML
+		);
+	};
+	Array.prototype.forEach.call(doc.querySelectorAll('script'), function(script) {
+		try {
+			if (script.textContent) {
+				win.run(script.textContent);
+			} else if (script.src) {
+				win.run(fs.readFileSync(path.join(__dirname, script.src)).toString());
+			}
+		} catch(e) {
+			console.error("Error while trying to execute script sourced by\n" + file);
+			process.exit(1);
+		}
+	});
+});
